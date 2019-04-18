489f8a1158daac4074bb42849796f8b4373c1193
Maier, Oliver <oliver.maier@tugraz.at>
2019-04-18 10:01:22 +0200

    Licensing

--- kernels/opencl_operator_kernels.c
+++ kernels/opencl_operator_kernels.c
@@ -1,4 +1,18 @@
+/*
+Copyright 2019 Oliver Maier
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 __kernel void operator_fwd(__global float2 *out, __global float2 *in,
                        __global float2 *coils, const int NCo,
                        const int NSl, const int NScan)
