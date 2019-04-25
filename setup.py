0bf483e085a54a5e284995956500444ba73cdd0a
Oliver Maier <oliver.maier@tugraz.at>
2019-04-25 09:59:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='rrsg_cgreco',
       version='0.1',
@@ -7,7 +7,7 @@ setup(name='rrsg_cgreco',
       author='Oliver Maier',
       author_email='oliver.maier@tugraz.at',
       license='Apache-2.0',
-      packages=setuptools.find_packages(),
+      packages=find_packages(),
       setup_requires=["cython"],
       install_requires=[
         'cython',
@@ -21,6 +21,6 @@ setup(name='rrsg_cgreco',
         'console_scripts': ['rrsg_cgreco = rrsg_cgreco._Init_CG:run',
                             'rrsg_plotresults = rrsg_cgreco._plot_results:run'],
         },
-        package_data={'rrsg_cgreco':['kernels/*.c']},
-        include_package_data=True,
+      package_data={'rrsg_cgreco':['kernels/*.c']},
+      include_package_data=True,
       zip_safe=False) 
